691ea9c5ffe2f1f055392b1584da45f1d2353f26
rafaelsankha6-boop <rafaelsankha6@gmail.com>
2026-04-16 23:54:48 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,17 +1,21 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "coder@2005",
-  database: "restaurant"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
+  ssl: {
+    rejectUnauthorized: true
+  }
 });
 
 db.connect((err) => {
   if (err) {
-    console.log("DB ERROR:", err);
+    console.log(" DB CONNECTION ERROR:", err.message);
   } else {
-    console.log("MySQL Connected 🚀");
+    console.log(" Cloud MySQL Connected Successfully ");
   }
 });
 
